78e6765b167d1a7645a81603e8a475ee2a4a4bdf
Tomat <xxlennygamerxx@gmail.com>
2023-09-02 11:04:59 -0700

    Project init.

---
+++ src/DegradationOfLIfe/DoLMod.cs
@@ -0,0 +1,7 @@
+﻿using JetBrains.Annotations;
+using Terraria.ModLoader;
+
+namespace DegradationOfLIfe;
+
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+public sealed class DoLMod : Mod { }
