61dad8dcee05a226c511c052d6feca956b8404db
Tomat <xxlennygamerxx@gmail.com>
2023-09-02 13:42:32 -0700

    Make the map more immersive.

---
+++ src/DegradationOfLife/Content/Features/ImmersiveMap/ImmersiveMapSystem.cs
@@ -0,0 +1,25 @@
+﻿using Terraria.Map;
+using Terraria.ModLoader;
+
+namespace DegradationOfLife.Content.Features.ImmersiveMap;
+
+public sealed class ImmersiveMapSystem : ModSystem {
+    public override void Load() {
+        base.Load();
+
+        On_WorldMap.UpdateLighting += ImmersivelyUpdateLighting;
+    }
+
+    private static bool ImmersivelyUpdateLighting(On_WorldMap.orig_UpdateLighting orig, WorldMap self, int x, int y, byte light) {
+        var other = self[x, y];
+        if (light == 0 && other.Light == 0)
+            return false;
+
+        var tile = MapHelper.CreateMapTile(x, y, light);
+        if (tile.Equals(ref other))
+            return false;
+
+        self.SetTile(x, y, ref tile);
+        return true;
+    }
+}
