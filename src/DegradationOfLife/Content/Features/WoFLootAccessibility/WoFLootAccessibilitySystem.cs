dfedc28d6d0d891081417579938d33c37f4d1038
Tomat <xxlennygamerxx@gmail.com>
2023-09-02 13:34:28 -0700

    Make WoF loot more accessible.

---
+++ src/DegradationOfLife/Content/Features/WoFLootAccessibility/WoFLootAccessibilitySystem.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace DegradationOfLife.Content.Features.WoFLootAccessibility;
+
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
+public sealed class WoFLootAccessibilitySystem : ModSystem {
+    public override void Load() {
+        base.Load();
+
+        On_NPC.CreateBrickBoxForWallOfFlesh += CreateAccessibleBrickBox;
+    }
+
+    private static void CreateAccessibleBrickBox(On_NPC.orig_CreateBrickBoxForWallOfFlesh orig, NPC self) { }
+}
