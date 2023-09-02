3fbf98d8dca4a5e85e97ac17ce46f68a94d89c47
Tomat <xxlennygamerxx@gmail.com>
2023-09-02 16:02:43 -0700

    Remove NPC happiness.

---
+++ src/DegradationOfLife/Content/Features/HappinessRemoval/HappinessRemovalPlayer.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace DegradationOfLife.Content.Features.HappinessRemoval;
+
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+internal sealed class HappinessRemovalPlayer : ModPlayer {
+    private int timer;
+
+    public override void Load() {
+        base.Load();
+
+        On_Main.OpenShop += ResetHappiness;
+    }
+
+    public override void PreUpdate() {
+        base.PreUpdate();
+
+        if (++timer % 4 == 0)
+            Player.currentShoppingSettings.PriceAdjustment *= 1.005f;
+    }
+
+    private static void ResetHappiness(On_Main.orig_OpenShop orig, Main self, int shopIndex) {
+        orig(self, shopIndex);
+        Main.LocalPlayer.currentShoppingSettings.PriceAdjustment = 1f;
+    }
+}
