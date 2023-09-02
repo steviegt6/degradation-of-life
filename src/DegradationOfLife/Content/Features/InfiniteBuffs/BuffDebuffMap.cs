613f98f65d69d6ea5611969a7b7166c4787d38a3
Tomat <xxlennygamerxx@gmail.com>
2023-09-02 11:34:03 -0700

    Begin implementing infinite buffs.

---
+++ src/DegradationOfLife/Content/Features/InfiniteBuffs/BuffDebuffMap.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DoL.Content.Features.InfiniteBuffs;
+
+internal sealed class BuffDebuffMap : ModSystem {
+    private readonly Dictionary<int, int> buffDebuffMap = new();
+
+    public override void Load() {
+        base.Load();
+
+        AddDebuff(BuffID.ObsidianSkin, -1); // TODO: effect?
+        AddDebuff(BuffID.Regeneration, -1); // TODO: halves player regeneration
+        AddDebuff(BuffID.Swiftness, -1); // TODO: decrease movement speed by 15%
+        AddDebuff(BuffID.Gills, -1); // TODO: drown faster
+        AddDebuff(BuffID.Ironskin, -1); // TODO: reduced defense (or dr?)
+        AddDebuff(BuffID.ManaRegeneration, -1); // TODO: halves mana regeneration
+        AddDebuff(BuffID.MagicPower, -1); // TODO: decreases magic damage by 10%
+        AddDebuff(BuffID.Featherfall, -1); // TODO: increase fall speed and gravity by 33%
+        AddDebuff(BuffID.Spelunker, -1); // TODO: effect?
+        AddDebuff(BuffID.Invisibility, -1); // TODO: increased aggro
+        AddDebuff(BuffID.Shine, -1); // TODO: dim lighting around the player significantly
+        AddDebuff(BuffID.NightOwl, -1); // TODO: blindness
+        AddDebuff(BuffID.Battle, -1); // TODO: no this is fine actually
+        AddDebuff(BuffID.Thorns, -1); // TODO: dealing damage to enemies damages you
+        AddDebuff(BuffID.WaterWalking, -1); // TODO: can't jump in water
+        AddDebuff(BuffID.Archery, -1); // TODO: decrease damage by 10%
+        AddDebuff(BuffID.Hunter, -1); // TODO: dim all npcs
+        AddDebuff(BuffID.Gravitation, -1); // TODO: flips randomly
+        AddDebuff(BuffID.Tipsy, -1); // TODO: only -4 defense
+        AddDebuff(BuffID.WellFed, BuffID.NeutralHunger);
+        AddDebuff(BuffID.Mining, -1); // TODO: mine slower
+        AddDebuff(BuffID.Heartreach, -1); // TODO: decrease range
+        AddDebuff(BuffID.Calm, -1); // TODO: zerg
+        AddDebuff(BuffID.Builder, BuffID.NoBuilding);
+        AddDebuff(BuffID.Titan, -1); // TODO: reduced kb
+        AddDebuff(BuffID.Flipper, -1); // TODO: increased water slow-down
+        AddDebuff(BuffID.Summoning, -1); // TODO: -1 minion slot
+        AddDebuff(BuffID.Dangersense, -1); // TODO: make dangerous tiles invisible
+        AddDebuff(BuffID.AmmoReservation, -1); // TODO: 20% chance to use extra ammo
+        AddDebuff(BuffID.Lifeforce, -1); // TODO: Math.Max(-100, health * .2), kills player at 100 hp
+        AddDebuff(BuffID.Endurance, -1); // TODO: -10% dr
+        AddDebuff(BuffID.Rage, -1); // TODO: -10% critical chance; negative critical chance can heal enemies
+        AddDebuff(BuffID.Inferno, BuffID.OnFire /*3*/);
+        AddDebuff(BuffID.Wrath, -1); // TODO: -10% damage
+        AddDebuff(BuffID.Fishing, -1); // TODO: -15 fishing power
+        AddDebuff(BuffID.Sonar, -1); // TODO: lie to the player
+        AddDebuff(BuffID.Crate, -1); // TODO: increase chance of getting trash instead
+        AddDebuff(BuffID.Warmth, BuffID.Frozen);
+        AddDebuff(BuffID.WellFed2, BuffID.Hunger);
+        AddDebuff(BuffID.WellFed3, BuffID.Starving);
+        AddDebuff(BuffID.Lucky, -1); // TODO: constant torch god fight
+    }
+
+    public void AddDebuff(int buffType, int debuffType) {
+        if (debuffType == -1)
+            return; // no-op; just for code consistency above
+
+        if (buffType < 0 || buffType > BuffLoader.BuffCount)
+            throw new ArgumentOutOfRangeException(nameof(buffType));
+
+        if ( /*debuffType < 0 ||*/ debuffType > BuffLoader.BuffCount)
+            throw new ArgumentOutOfRangeException(nameof(debuffType));
+
+        buffDebuffMap[buffType] = debuffType;
+    }
+
+    public int? GetDebuffFromBuff(int buffType) {
+        return buffDebuffMap.TryGetValue(buffType, out var debuffType) ? debuffType : null;
+    }
+}
--- src/DegradationOfLife/DoLMod.cs
+++ src/DegradationOfLife/DoLMod.cs
@@ -1,7 +1,35 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using System.Reflection;
+using DoL.Content.Features.InfiniteBuffs;
+using JetBrains.Annotations;
 using Terraria.ModLoader;
 
 namespace DoL;
 
 [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
-public sealed class DoLMod : Mod { }
+public sealed class DoLMod : Mod {
+    public override object? Call(params object?[] args) {
+        var command = ExpectCallArg<string>(0, args).ToLower();
+
+        switch (command) {
+            case "addbuffdebufmap": {
+                var buffId = ExpectCallArg<int>(1, args);
+                var debuffId = ExpectCallArg<int>(2, args);
+                ModContent.GetInstance<BuffDebuffMap>().AddDebuff(buffId, debuffId);
+                break;
+            }
+        }
+
+        return base.Call(args);
+    }
+
+    private static T ExpectCallArg<T>(int index, object?[] args) {
+        if (args.Length <= index)
+            throw new TargetParameterCountException();
+
+        if (args[index] is not T obj)
+            throw new InvalidCastException();
+
+        return obj;
+    }
+}
